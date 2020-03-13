15f02a879030c6df1d915b0ab8037999554b67f7
Richard Janssen <rxpjanssen@hotmail.com>
2020-03-13 20:38:14 +0100

    added the three different implementations

--- source/ExternalDLL/ExternalDLL/StudentPreProcessing.cpp
+++ source/ExternalDLL/ExternalDLL/StudentPreProcessing.cpp
@@ -7,8 +7,21 @@ IntensityImage * StudentPreProcessing::stepToIntensityImage(const RGBImage &imag
 	for (uint_fast64_t y = 0; y < image.getHeight(); y++) {
 		for (uint_fast64_t x = 0; x < image.getWidth(); x++) {
 			RGB tmpPixel = image.getPixel(x,y);
-			Intensity intensity = (double(tmpPixel.r) + double(tmpPixel.g) + double(tmpPixel.b)) / 3;
-			intensityImage->setPixel(x, y, intensity);
+			//standaard, (R+G+B)/3
+			if (false) {
+				intensityImage->setPixel(x, y,
+					(double(tmpPixel.r) + double(tmpPixel.g) + double(tmpPixel.b)) / 3);
+			}
+			// R * (1/3), G * (1/2), B * (1/6)
+			else if (false) {
+				intensityImage->setPixel(x, y,
+					double(tmpPixel.r) * (1.f / 3.f) + double(tmpPixel.g) * (0.5) + double(tmpPixel.b) * (1.f / 6.f));
+			}
+			// R * 30%, G * 59%, B * 0.11%
+			else if (true) {
+				intensityImage->setPixel(x, y,
+					double(tmpPixel.r) * (0.30) + double(tmpPixel.g) * (0.59) + double(tmpPixel.b) * (0.11));
+			}
 		}
 	}
 	//intensityImage->setPixel(0, 0, 0); //test if actually being used
